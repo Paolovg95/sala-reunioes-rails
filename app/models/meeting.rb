0169511c9c8299eab0f11be374099856215157ec
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-03-27 22:44:11 -0300

    Home Views and New Form

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,7 @@
 class Meeting < ApplicationRecord
+  TIMES = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+  DAYS = ["Segunda","Terca","Quarta","Quinta","Sexta"]
 
   has_one :slot
+  has_one :user, through: :slots
 end
