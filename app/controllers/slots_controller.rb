1c4c48e01dfc8befeab0eeccb635e39a6c3071c8
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-03-27 22:02:51 -0300

    Meeting & Slots Controllers

---
+++ app/controllers/slots_controller.rb
@@ -0,0 +1,31 @@
+class SlotsController < ApplicationController
+  before_action :find_meeting, only: [:new, :create]
+
+  def new
+    @slot = Slot.new
+  end
+
+  def create
+    @slot = Slot.create(slot_params)
+    @slot.meeting = @meeting
+    @slot.user = current_user
+
+    if @slot.save
+      redirect_to :root
+    else
+      render :new
+    end
+
+  end
+
+
+  private
+  def slot_params
+    params.require(:slot).permit(:title, :description, :length)
+
+  end
+
+  def find_meeting
+    @meeting = Meeting.find(params[:meeting_id])
+  end
+end
