a97dc66e129f57b520c75edc62b85e3310fc4254
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-03-27 22:48:13 -0300

    Seeds Databse for Times and Days

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Meeting.destroy_all
+
+  Meeting::DAYS.each do |day|
+    Meeting::TIMES.each do |hour|
+      meeting = Meeting.create(
+        day: day,
+        time: hour
+      )
+    end
+  end
