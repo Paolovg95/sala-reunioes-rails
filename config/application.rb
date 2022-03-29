99fe145f3bd4c5482caa49c51fefc730e9ca08ea
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-03-28 23:28:27 -0300

    Time Zone Added

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SalaReunionRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_view.embed_authenticity_token_in_remote_forms = true
-
+    config.time_zone = "America/Sao_Paulo"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
