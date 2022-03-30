31b3320e3d0aba067341d924e528bb2e718ba449
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-03-29 22:47:42 -0300

    Authenticate Users except on Home View

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,4 +1,6 @@
 class MeetingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @meetings = policy_scope(Meeting)
   end
@@ -6,6 +8,7 @@ class MeetingsController < ApplicationController
   def show
     @meeting = Meeting.find(params[:id])
     authorize @meeting
+
   end
 
 end
