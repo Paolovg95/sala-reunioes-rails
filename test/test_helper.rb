7a412950c0ab5a3557c5b83815240b45c7805194
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-03-30 23:52:28 -0300

    Capybara DSL Test Added

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'capybara/rails'
+require 'capybara/minitest'
 
 class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
-
+  include Capybara::SessionMatchers
   include Warden::Test::Helpers
   Warden.test_mode!
 
@@ -16,4 +18,18 @@ class ActiveSupport::TestCase
   end
   Capybara.save_path = Rails.root.join('tmp/capybara')
   Capybara.javascript_driver = :headless_chrome
+
+end
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  # Make `assert_*` methods behave like Minitest assertions
+  include Capybara::Minitest::Assertions
+
+  # Reset sessions and driver between tests
+  teardown do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
 end
