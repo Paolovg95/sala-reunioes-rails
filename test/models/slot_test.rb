f833b124d755a7b5e023d077f7fe914e038c6354
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-03-31 12:53:53 -0300

    Slot Model Test

--- test/models/slot_test.rb
+++ test/models/slot_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class SlotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Not Allow Creating Slots without Title" do
+    slot = Slot.new
+    slot.description = "123456"
+    slot.length = 15
+    assert_not slot.save, "Saved"
+  end
+
+  test "Not Allow Creating Slots without Description" do
+    slot = Slot.new
+    slot.title = "Lorem ipsum"
+    slot.length = 15
+    assert_not slot.save, "Saved"
+  end
+
+  test "Not Allow Creating Slots without Length" do
+    slot = Slot.new
+    slot.title = "Lorem"
+    slot.description = "Lorem ipsum patr vitri"
+    assert_not slot.save, "Saved"
+  end
+
+
 end
