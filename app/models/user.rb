476d6747af9469f92197999712b2589ec7624113
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-04-01 01:18:42 -0300

    Models and Controllers Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates_presence_of :first_name, :email, :password, :password_confirmation
+  validates :email, uniqueness: true
+  validates :first_name, uniqueness: true
 
-  has_many :slots, dependent: :destroy
+  has_many :slots, dependent: :delete_all
 end
